82023f8493b7b92feff5bb32fc5cc053fd1c4fff
Weiyixmm <634482545@qq.com>
2020-09-07 18:46:39 +0800

    other modify

--- src/public/index.php
+++ src/public/index.php
@@ -3,7 +3,7 @@
 /* 定义这个常量是为了在application.ini中引用*/
 define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../'));
 
-include dirname(APPLICATION_PATH) . "/vendor/autoload.php";
+include dirname(APPLICATION_PATH) . '/vendor/autoload.php';
 include APPLICATION_PATH . '/application/Initialization.php';
 
 $application = new \Yaf\Application( APPLICATION_PATH . '/conf/application.ini', 'product');
